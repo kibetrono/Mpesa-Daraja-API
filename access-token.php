1842a0825651d7ae57701a051ea88c3967a085d7
Kibet <kibetdavidro@gmail.com>
2023-02-23 11:27:31 +0300

    add access token file

---
+++ access-token.php
@@ -0,0 +1,22 @@
+<?php
+// consumer key and secret
+$consumerKey = 'O0GCydr4wLTPFkLmzuUR2OIXmNFPHzKW';
+$consumerSecret = 'easwMa5WVnyJ0daW';
+
+// access token url
+$access_token_url = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials';
+# header for access token
+$headers = ['Content-Type:application/json; charset=utf8'];
+
+
+$curl = curl_init($access_token_url);
+curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+curl_setopt($curl, CURLOPT_HEADER, FALSE);
+curl_setopt($curl, CURLOPT_USERPWD, $consumerKey.':'.$consumerSecret);
+$result = curl_exec($curl);
+$status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+$result = json_decode($result);
+$access_token = $result->access_token;  
+
+curl_close($curl);
